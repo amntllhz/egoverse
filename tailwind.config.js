98b5665a68befd606632d838ceb0fb335e3706eb
amntllhz <amanatullah1904@gmail.com>
2024-08-09 10:49:17 +0700

    init front

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  theme: {
+    extend: {
+      fontFamily : {
+        clash : "'Clash Display', sans-serif",
+      },
+      colors : {
+        back : '#f1f4f5',
+      },
+    },
+  },
+  plugins: [],
+}
+
