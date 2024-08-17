00072f5b209f70672e16490be36e1bd5df9e5063
amntllhz <amanatullah1904@gmail.com>
2024-08-17 16:29:54 +0700

    update submit form sore

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   const copyButton = document.querySelector("#copy-button");
   const resiText = document.querySelector("#resi-text");
+  const copyIcon = document.querySelector("#copy-icon");
+  const checkedIcon = document.querySelector("#checked-icon");
 
   copyButton.addEventListener("click", (event) => {
     event.preventDefault(); // Mencegah perilaku default anchor tag
+
     navigator.clipboard
       .writeText(resiText.textContent)
       .then(() => {
-        alert("Copied to clipboard!");
+        // Ganti ikon setelah salin berhasil
+        copyIcon.style.display = 'none';
+        checkedIcon.style.display = 'block';
+
+        // Kembalikan ikon setelah 2 detik
+        setTimeout(() => {
+          copyIcon.style.display = 'block';
+          checkedIcon.style.display = 'none';
+        }, 5000);
       })
       .catch((err) => {
-        console.error("Failed to copy: ", err);
+        console.error("Gagal menyalin: ", err);
       });
   });
 });
