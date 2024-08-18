d9d2f154fecf67a831ef7265ad9cd446feeb8817
amntllhz <amanatullah1904@gmail.com>
2024-08-18 10:43:19 +0700

    rensponsive on 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
         back : '#f1f4f5',
       },
     },
+    screens: {
+      'sm': {'max':'640px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': {'min': '641px','max':'768px'},
+      // => @media (min-width: 768px) { ... }
+
+      'lg': {'min': '769','max':'1024px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': {'min': '1025','max':'1280px'},
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': {'min': '1281px','max':'1536px'},
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
