93d8356e1203d232b5e88a1572ac0b74e80224a2
amntllhz <amanatullah1904@gmail.com>
2024-09-02 21:06:26 +0700

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html',
-    './node_modules/flowbite/**/*.js',
+  content: ['../src/**/*.html',
+    '../node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
